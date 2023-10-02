6b3fcce7940fbbdadd79314262ad851cc07a72ca
Logan Johnson <logan01@iastate.edu>
2023-10-02 16:40:02 -0500

    updates to code

--- Test_Mapping.R
+++ Test_Mapping.R
@@ -1,15 +1,10 @@
 library(tidyverse)
 library(tidygeocoder)
+library(leaflet)
 
+# This is power plant data from the WRI that is last updated in 2018
 
-
-data <- readr::read_csv(file = "global_power_plant_database.csv")
-
-data <- data %>%
-  dplyr::filter(country_long == "United States of America")
-
-data_1 <- data %>%
-  tidygeocoder::geocode(full_address, method = 'arcgis', lat = latitude, long = longitude)
+data <- readr::read_csv(file = "usa_power_plant_data_world_resources_institure.csv")
 
 plot <- leaflet::leaflet() %>%
   leaflet::addProviderTiles("Stamen.Toner") %>%
@@ -20,6 +15,32 @@ plot <- leaflet::leaflet() %>%
     label = lapply(data$name, htmltools::HTML),
     labelOptions = leaflet::labelOptions(noHide = F, direction = 'auto'),
     stroke = F,
-    fillColor = "navy"
+    fillColor = "navy",
+    radius = 5
   )
 plot
+
+
+# Takes in the shape file and returns a dataframe
+
+## This is the Iowa CAFO Data that was last updated in
+
+my_spdf <- sf::st_read(
+  dsn = "~/Downloads/animal_feeding_operations/"
+)
+
+# readr::write_csv(test, file = "iowa_cafo_data.csv")
+
+plot_1 <- leaflet::leaflet() %>%
+  leaflet::addProviderTiles("Stamen.Toner") %>%
+  leaflet::addCircleMarkers(
+    data = my_spdf,
+    lat = ~ Latitude,
+    lng = ~Longitude,
+    label = lapply(my_spdf$FacilityNa, htmltools::HTML),
+    labelOptions = leaflet::labelOptions(noHide = F, direction = 'auto'),
+    stroke = F,
+    fillColor = "navy",
+    radius = 5
+  )
+plot_1
